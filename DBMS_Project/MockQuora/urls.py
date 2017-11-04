0573b4f17616bd8cc2ebe3c7cf4f0676f1dd9797
Shivam <shivamjindal39@gmail.com>
2017-11-04 23:29:55 +0530

    Complete Feed and follow views

--- DBMS_Project/MockQuora/urls.py
+++ DBMS_Project/MockQuora/urls.py
@@ -5,13 +5,13 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
-    url(r'^login/$', views.user_login, name='login'),
-    url(r'^add_details/$', views.add_profile_details, name='add_details'),
-    url(r'^register/$', views.register_user, name='register'),
-    url(r'^feed/$', views.feed, name='feed'),
-    url(r'^post_question/$', views.post_question, name='post_question'),
-    url(r'^question/(?P<question_id>\d+)/$', views.question_page, name='question_page'),
-    url(r'^profile/(?P<user_id>\d+)/$', views.profile, name='profile'),
-)
-
+                       url(r'^$', views.index, name='index'),
+                       url(r'^login/$', views.user_login, name='login'),
+                       url(r'^add_details/$', views.add_profile_details, name='add_details'),
+                       url(r'^register/$', views.register_user, name='register'),
+                       url(r'^feed/$', views.feed, name='feed'),
+                       url(r'^post_question/$', views.post_question, name='post_question'),
+                       url(r'^question/(?P<question_id>\d+)/$', views.question_page, name='question_page'),
+                       url(r'^follow/(?P<follow_id>\d+)/(?P<followed_id>\d+)/$', views.follow, name='follow'),
+                       url(r'^profile/(?P<user_id>\d+)/$', views.profile, name='profile'),
+                       )
